a7007474e81a59cce86c3fcb79ba67d528cd5e1c
Rahul Dahal <rdaahal@gmail.com>
2021-11-12 17:26:49 +0545

    feat: read todos from localstorage

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
 (function () {
+  // Add todo function
+  const todoList = document.querySelector(".todo__list");
+  function addTodo(value) {
+    if (todoList.childNodes[0].nodeName === "#text") {
+      todoList.innerHTML = "";
+    }
+    const listItem = document.createElement("li");
+    listItem.classList.add("todo__listItem");
+
+    const listButton = document.createElement("button");
+    listButton.classList.add("todo__listButton");
+
+    const listIconContainer = document.createElement("span");
+    listIconContainer.classList.add("checkIconContainer");
+
+    const listValue = document.createElement("span");
+    listValue.textContent = value;
+
+    listButton.appendChild(listIconContainer);
+    listButton.appendChild(listValue);
+    listItem.appendChild(listButton);
+    todoList.appendChild(listItem);
+  }
+
+  // Save to LocalStorage
+  const localStorageKey = "TODOS";
+  function saveToLocalStorage(value) {
+    todos.push(value);
+    localStorage.setItem(localStorageKey, JSON.stringify(todos));
+  }
+
+  // Read todos
+  const todos = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+  if (todos.length === 0) {
+    todoList.textContent = "There are no todos left.";
+  } else {
+    todos.forEach((todo) => {
+      addTodo(todo);
+    });
+  }
+
   // Theme toggle
   const todoElement = document.querySelector(".todo");
   const themeToggler = document.querySelector(".todo__themeToggler");
@@ -8,35 +49,13 @@
   );
 
   // Add todo item
-  const todoList = document.querySelector('.todo__list');
-  const todoForm = document.querySelector('.todo__form');
-  const todos = [];
-  const localStorageKey = 'TODOS';
+  const todoForm = document.querySelector(".todo__form");
 
-  todoForm.addEventListener('submit', e=>{
+  todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const {value} = todoForm[0];
+    const { value } = todoForm[0];
     todoForm.reset();
-
-    const listItem = document.createElement('li');
-    listItem.classList.add('todo__listItem');
-
-    const listButton = document.createElement('button');
-    listButton.classList.add('todo__listButton');
-
-    const listIconContainer = document.createElement('span');
-    listIconContainer.classList.add('checkIconContainer');
-  
-    const listValue = document.createElement('span');
-    listValue.textContent = value;
-    
-    listButton.appendChild(listIconContainer);
-    listButton.appendChild(listValue);
-    listItem.appendChild(listButton);
-    todoList.appendChild(listItem);
-
-    // Save to LocalStorage
-    todos.push(value);
-    localStorage.setItem(localStorageKey, JSON.stringify(todos)); 
-  })
+    addTodo(value);
+    saveToLocalStorage(value);
+  });
 })();
